ebaec46bed36286d98553b3d43584fd910222f53
unknown <tiago.blobao@gmail.com>
2018-03-11 13:07:35 -0300

    Adicionando arquivo principal

---
+++ main.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name SipacEletrica_V2
+// @namespace https://github.com/tiagolobao/SipacEletrica_V2
+// @version 2.0
+// @description Script para gerenciamento das ordens de serviço de forma automática no NOVO site do SIPAC-UFBA
+// @author Tiago Britto Lobão
+// @match https://sipac.ufba.br/sipac/supinfra/manutencao/cadastraOS.jsf*
+// @grant none
+// @require http://code.jquery.com/jquery-latest.js
+// @require https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.2.61/jspdf.min.js
+// ==/UserScript==
+
+(function() {
+
+  
+  console.log("adieu");
+})();
