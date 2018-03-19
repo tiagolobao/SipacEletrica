44b5a608542dcbca546c28c96b4308b788cb2b3e
Tiago Britto Lobão <tiago.blobao@gmail.com>
2018-03-19 15:28:10 -0300

    Ajuste @match

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // @version 2.0
 // @description Script para gerenciamento das ordens de serviço de forma automática no NOVO site do SIPAC-UFBA
 // @author Tiago Britto Lobão
-// @match https://sipac.ufba.br/sipac/supinfra/manutencao/cadastraOS.jsf*
+// @match https://sipac.ufba.br/*
 // @grant none
 // @require http://code.jquery.com/jquery-latest.js
 // @require https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.2.61/jspdf.min.js
