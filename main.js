43b6b9c5bdadae7cbff8db4f1960eaeff6e8deef
Tiago Britto Lobão <tiago.blobao@gmail.com>
2018-03-19 21:14:55 -0300

    <Vários Ajustes>
    1 - Debug para a utilização no google chrome
    2 - Adição de uma condição para execução do script onde deverá estar em
    processo de finalização para funcionar, evitando que se finalize ordens
    de serviço em momentos que não for solicitado

--- main.js
+++ main.js
@@ -44,7 +44,7 @@
          input = input.replace(/ /g,''); //Removendo todos os espaços
          sessionStorage.setItem("requisit",input);
          sessionStorage.setItem("acessos",0);
-         sessionStorage.setItem("passo",0);
+         sessionStorage.setItem("processoFinalizaAuto",true);
          window.location.href = enderecoBuscaOsPreBusca;
       });
    }
@@ -53,41 +53,67 @@
          CÓDIGO PARA A PÁGINA CONSULTA OS CASO TENHA CLICADO PARA FINALIZAR
    *************************************************************************/
    else if (window.location.href == enderecoBuscaOsPreBusca  ||  window.location.pathname.indexOf("populaOS") > -1){
-      /* Realizando uma nova busca de OS */
-      var requisit = new Array();
-      var acess = sessionStorage.getItem("acessos");
-      var input = sessionStorage.getItem("requisit");
 
-      var j=0;
-      var anterior=0;
-      input = ' ' + input; //Evitando que a primeira execução da substring exclua o primeiro caractere
-      input = input + ','; //Evitando que o algoritimo não pegue a ultima requisitão
-      for(i=0;i<=input.length;i++){
-         if(input[i] == ","){
-            requisit[j] = input.substring(anterior+1, i); //Separando em uma array cada requisição
-            j = j + 1; //Contando o número de requisições
-            anterior = i;
-         }
-      }
+      //Verifica se o processo de finalização automatica está ativo
+      let processoFinalizaAuto = sessionStorage.getItem("processoFinalizaAuto");
+      if(processoFinalizaAuto){
+         /* Realizando uma nova busca de OS */
+         var requisit = [];
+         var acess = parseInt( sessionStorage.getItem("acessos") );
+         var input = sessionStorage.getItem("requisit");
 
-      /* Caso ainda existam requisições a serem finalizadas */
-      if(acess<=requisit.length){
-         sessionStorage.setItem("acessos",acess+1);
-         for(i=0;i<=requisit[acess].length;i++){
-            if(requisit[acess][i]=='/'){
-                  barra=i; //localizando a posição da barra que separa o numero da requisição do ano
+         var j=0;
+         var i;
+         var anterior=0;
+         input = ' ' + input; //Evitando que a primeira execução da substring exclua o primeiro caractere
+         input = input + ','; //Evitando que o algoritimo não pegue a ultima requisitão
+         for(i=0;i<=input.length;i++){
+            if(input[i] == ","){
+               requisit[j] = input.substring(anterior+1, i); //Separando em uma array cada requisição
+               j = j + 1; //Contando o número de requisições
+               anterior = i;
             }
          }
-         jQuery("#consultaPorRequisicaoCheck").click();
-         jQuery("input[name='ordemServico.requisicao.numero']").val(requisit[acess].substring(0,barra)); //numero da req
-         jQuery("input[name='ordemServico.requisicao.ano']").val(requisit[acess].substring(barra+1,requisit[acess].length+1)); //ano da req
-         setTimeout(function(){ jQuery( "#conteudo > form > table > tfoot >tr > td > input:nth-child(2)" ).click(); }, 100);
+
+         /* Caso ainda existam requisições a serem finalizadas */
+         if(acess<=requisit.length){
+            var barra;
+            sessionStorage.setItem("acessos",acess+1);
+            for(i=0;i<=requisit[acess].length;i++){
+               if(requisit[acess][i]=='/'){
+                     barra=i; //localizando a posição da barra que separa o numero da requisição do ano
+               }
+            }
+            jQuery("#consultaPorRequisicaoCheck").click();
+            jQuery("input[name='ordemServico.requisicao.numero']").val(requisit[acess].substring(0,barra)); //numero da req
+            jQuery("input[name='ordemServico.requisicao.ano']").val(requisit[acess].substring(barra+1,requisit[acess].length+1)); //ano da req
+            setTimeout(function(){ jQuery( "#conteudo > form > table > tfoot >tr > td > input:nth-child(2)" ).click(); }, 100);
+         }
+         /* Caso não exista mais requisições a serem finalizadas */
+         else{
+            sessionStorage.clear();
+            alert("As requisições foram finalizadas");
+         }
       }
-      /* Caso não exista mais requisições a serem finalizadas */
-      else{
-         storage.clear();
-         alert("As requisições foram finalizadas");
-      }
+   }
+
+   /*************************************************************************
+         CÓDIGO PARA CLICAR EM ALTERAR OS CASO ESTEJA NO PROCESSO DE FINALIZAÇÃO AUTOMATICA
+   *************************************************************************/
+   if (window.location.pathname.indexOf("buscaOS") > -1){
+         //Espera a página terminar de carregar
+         jQuery(document).ready( function() {
+            /* Confere se é o momento certo de clicar para alterar a OS
+               1 - Deve estar ativo o processo de finalização automatica
+               2 - Deve acontecer apenas depois que buscar a OS correta a se finalizar
+            */
+            let enderecoPaginaAtual = window.location.href; //1
+            let processoFinalizaAuto = sessionStorage.getItem("processoFinalizaAuto"); //2
+            if(enderecoPaginaAtual != enderecoBuscaOsPreBusca && processoFinalizaAuto){
+               let url = jQuery("#conteudo > table > tbody > tr >  td:nth-child(9) > a ").prop("href");
+               jQuery(location).attr("href", url);
+            }
+      });
    }
 
   console.log("adieu");
